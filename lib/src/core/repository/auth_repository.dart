23be4e4b340daaa17dfe0b77be1ebdd4e541a0c1
Digital Shreee <shreeecreation720@gmail.com>
2024-01-30 16:25:28 +0545

    feat : audio added

--- lib/src/core/repository/auth_repository.dart
+++ lib/src/core/repository/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:motivational/src/core/clients/pocket_base_client.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 final class AuthRepository {
   final pb = PocketBaseClient();
@@ -6,7 +7,12 @@ final class AuthRepository {
   bool get isSignedIn => pb.client.authStore.model != null;
 
   Future<void> signInWithGoogle() async {
-    pb.client.collection('users').authWithOAuth2('google', (url) {
+    pb.client.collection('users').authWithOAuth2('google', (url) async {
+      try {
+        await launchUrl(url);
+      } catch (e) {
+        print(e.toString());
+      }
     });
   }
 }
